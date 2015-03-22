c99596c7601dd401588b3bfa23a5f7ef82a8b00c
Johan Andrén <johan.andren@mejsla.se>
2015-03-22 15:24:38 +0100

    Forgot some traversal tests in the sequence spec when refactoring

--- src/test/scala/markatta/futiles/SequencingSpec.scala
+++ src/test/scala/markatta/futiles/SequencingSpec.scala
@@ -91,35 +91,4 @@ class SequencingSpec extends Spec {
 
   }
 
-
-  describe("traverse sequentially") {
-
-    it("executes the futures sequentially") {
-      val latch = new CountDownLatch(3)
-      val result = traverseSequentially(List(1,2,3)) { n =>
-        Future {
-          latch.countDown()
-          val l = latch.getCount.toInt
-          (n, l)
-        }
-      }
-
-      result.futureValue should be (List((1,2), (2,1), (3,0)))
-    }
-
-    it("fails if one of the futures fails") {
-      val latch = new CountDownLatch(2)
-      val result = traverseSequentially(List(1,2,3)) { n =>
-        Future {
-          latch.countDown()
-          val l = latch.getCount.toInt
-          if (l == 0) throw new RuntimeException("fail")
-          (n, l)
-        }
-      }
-
-      Lifting.liftTry(result).futureValue.isFailure should be (true)
-    }
-
-  }
 }
