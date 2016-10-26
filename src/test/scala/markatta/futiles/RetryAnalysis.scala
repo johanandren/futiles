53b6462ada8b6c7bfe9ab5e481fb58a7f22eab63
Johan Andrén <johan@markatta.com>
2016-10-26 18:35:54 +0200

    Bugfix #10, retry algo was incorrectly documented and itself also incorrect

---
+++ src/test/scala/markatta/futiles/RetryAnalysis.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
+ */
+package markatta.futiles
+
+import java.io.File
+
+import scala.concurrent.duration._
+
+object RetryAnalysis extends App {
+
+  val file = new File("./data.tsv")
+  val p = new java.io.PrintWriter(file)
+
+  val range = (1 to 10)
+  val unit = 1.second
+
+  val result = range.flatMap { n =>
+    (0 to 1000).map(_ => p.println(n + "\t" + Retry.nextBackOff(n, unit).toMillis))
+  }
+
+  println(s"Wrote $file")
+
+
+
+
+}
